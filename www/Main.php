10854614fdc18d578b7b32015071c541318d0eb6
tve <tve@karelia.ru>
2019-01-26 18:28:06 +0300

    Первое заполнение галереи новостей

--- www/Main.php
+++ www/Main.php
@@ -7,7 +7,7 @@
 
 //                                                   Автор:       Труфанов В.Е.
 //                                                   Дата создания:  13.01.2019
-// Copyright © 2019 tve                              Посл.изменение: 15.01.2019
+// Copyright © 2019 tve                              Посл.изменение: 26.01.2019
 
 ?>
 <!DOCTYPE html>
@@ -19,30 +19,20 @@
    <meta name="description" content="Труфанов Владимир Евгеньевич, его жизнь и жизнь его близких">
    <meta name="keywords" content="Труфанов Владимир Евгеньевич, жизнь и увлечения">
    <link rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,400i,700,700i&amp;subset=cyrillic">
+      href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,400i,700,700i&amp;
+      subset=cyrillic">
    <link rel="stylesheet" type="text/css" href="Styles/Reset.css">
    <link rel="stylesheet" type="text/css" href="Styles/Styles.css">
    <link rel="stylesheet" type="text/css" href="Styles/ImgRight.css">
+
    <link rel="stylesheet" type="text/css" href="TJsPrown/TJsPrown.css">
-
-   
-   <style>
-
-</style>
- 
-<script
-   src="https://code.jquery.com/jquery-3.3.1.min.js"
-   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-   crossorigin="anonymous">
-</script>
-<script src="/TJsPrown/TJsPrown.js"></script>
-<script> MakeCatchyQuotes() </script>
-
-<!--
--->
-
-   
-   
+   <script
+      src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous">
+   </script>
+   <script src="/TJsPrown/TJsPrown.js"></script>
+   <script> MakeCatchyQuotes() </script>
 </head>
 
 <body>
@@ -50,21 +40,40 @@
    -->
    <div class="Gallery">
       <div class="Card">
-         <img class="ii" 
+         <img class="imgCard" 
          src="/Pages/tveArc1/tve1-1-pod^yom-nastroeniya.jpg" 
          alt="Подъём настроения">
-         <p class="pppi"> 
+         <p class="pCard"> 
             <?php
             require_once "Pages/tveArc1/tve1-1-comm.txt";
             ?> 
          </p>
       </div>
-      <div class="Card">2</div>
-      <div class="Card">3</div>
-      <img class="ii" src="/Images/na-sampo.jpg">
-      <div class="Card">14</div>
-      <div class="Card">21 First</div>
-      <div class="Card">35</div>
+      <div class="Card">
+         <img class="imgCard" 
+         src="/Images/С заботой и к мамам.jpg" 
+         alt="С заботой и к мамам">
+      </div>
+      <div class="Card">
+         <img class="imgCard" 
+         src="/Images/nasampo.jpg" 
+         alt="На горе Сампо">
+      </div>
+      <div class="Card">
+         <img class="imgCard" 
+         src="/Images/dozhd'-prazdniku-ne-pomeha.jpg" 
+         alt="Дождь празднику не помеха">
+      </div>
+      <div class="Card">
+         <img class="imgCard" 
+         src="/Images/uyolochki.png" 
+         alt="У ёлочки">
+      </div>
+      <div class="Card">
+         <img class="imgCard" 
+         src="/Images/hochesh'-kvasku.jpg" 
+         alt="Хочешь кваску">
+      </div>
    </div>
 
    <section class="News">
