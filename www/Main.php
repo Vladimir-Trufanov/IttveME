4478fa0a3ad455655e30b26d0a319f123ca60901
tve <tve@karelia.ru>
2019-02-19 20:36:43 +0300

    Выделен модуль Site.php

--- www/Main.php
+++ www/Main.php
@@ -51,83 +51,10 @@ if ($PersName<>$UserName)
 
 // Разворачиваем страницу
 require_once "iHtmlBegin.php";
+\prown\MakeUserError('PersEntry1214',1);
 
-\prown\MakeUserError('PersEntry121');
-
-
-?>
-<!-- 
--->
-
-
-
-<div id="hello" title="Привет, Мир!">
-				<p>Это диалоговое окно на самом деле является тегом div, размещенным на странице с помощью абсолютного позиционирования.</p>
-				<p>Попробуйте перетащить диалоговое окно по экрану. Вы можете это сделать!</p>
-			</div>
-  
-<div class="Ers">
-   Это появившаяся b ошибка!
-</div>
-
-<div class="Gallery">
-   <?php
-   require_once "GalleryView.php";
-   ?> 
-</div>
-
-<section class="News">
-   <?php
-   require_once "Pages/tveArt/tve2-114-po-trope-k-karnizu-reki-bzerp'.html";
-   ?>
-</section>
-
-<section class="Life">
-   <?php
-   require_once "Pages/tveArc1/tve1-1-оsobennosti-ustrojstva-vintikov-v-moej-golove.html";
-   ?>
-</section>
-  
-<div class="Footer">
-   <div class="LeftFooter">
-      <?php 
-         //echo $SiteDevice/*.': '.$SiteRoot.'-'.$SiteAbove.'-'.$SiteHost*/; 
-         echo $uagent.'<br>';
-      ?>
-   </div>
-   <div class="LifeMenu">
-      <button class="btnLifeMenu">
-         <img class="imgLifeMenu" src="/Images/Buttons/tveMenuD.png" alt="tveMenuD">
-      </button>
-   </div>
-   <div class="RightFooter">
-      <?php
-      require_once "NavSet.php";
-      ?>
-   </div>
-</div>
-  
-<div class="Ext">
-   Main
-</div>
-  
-<div class="Info">
-   <div class="InfoLeft">
-      Copyright (c) 2019 Труфанов Владимир   tve58@inbox.ru<br>
-      <?php 
-         //echo $SiteDevice/*.': '.$SiteRoot.'-'.$SiteAbove.'-'.$SiteHost*/; 
-         //echo $uagent.'<br>';
-      ?>
-   </div>
-   <div class="InfoRight">
-      <?php 
-         echo $SiteDevice.
-         " ".$PersName." ".$_SESSION['Counter'].".".$PersEntry."[".$BrowEntry."]"; 
-      ?>
-   </div>
-</div>
-
-<?php
+// Выполняем начальную инициализацию
+require_once "Site.php";
 
 require_once "iHtmlEnd.php";
 
