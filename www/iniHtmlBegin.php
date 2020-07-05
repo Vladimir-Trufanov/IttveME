ef06b2c3720113dfb449d18ec9c1a0ef97107d64
Vladimir-Trufanov <tve@karelia.ru>
2020-07-05 18:19:00 +0300

    Подключен Favicon

--- www/iniHtmlBegin.php
+++ www/iniHtmlBegin.php
@@ -135,6 +135,20 @@ else
    <?php
 
 }
+
+// Данные о favicon
+echo '
+<link rel="manifest" href="manifest.json">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon260x260/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon260x260/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon260x260/favicon-16x16.png">
+<link rel="mask-icon" href="/favicon260x260/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/favicon260x260/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="/favicon260x260/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+';
+
 echo '</head>';
 echo '<body>';
 
