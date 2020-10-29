13e6b4eb78b6ad7fe9a2ed47770b56bf3aec9c46
Vladimir-Trufanov <tve@karelia.ru>
2020-10-29 22:18:14 +0300

    Начато ini_ResCookie(), нужно переделать через таблицу

--- www/Inimem.php
+++ www/Inimem.php
@@ -14,8 +14,9 @@
 define ("ChangeSize", "chs");    // "Изменить размер базового шрифта"  
 // ---------------------- Регулятор кукисов (порядок использования кукисов) ---
 define ("rciCookiNo", 0);        // кукисов нет
-define ("rciCookiUserNo", 1);    // есть, пользователем запрещено использование
-define ("rciCookiUserYes", 2);   // пользователем разрешено использование кукисов
+define ("rciCookiNoMes", 1);     // кукисов нет, выдать сообщение
+define ("rciCookiUserNo", 2);    // есть, пользователем запрещено использование
+define ("rciCookiUserYes", 3);   // пользователем разрешено использование кукисов
 // ---------------------------------------- Режимы представления материалов ---
 define ("rpmDoubleRight", 0);    // двухколоночный с правосторонней галереей
 define ("rpmDoubleLeft",  1);    // двухколоночный с левосторонней галереей
@@ -25,12 +26,63 @@ define ("rpmOneLeft",     3);    // одноколоночный с левост
 define ("vexLoginPage",   0);    // отдельная страница при входе
 define ("vexSeparImage",  1);    // увеличенное изображение выбранной картинки 
 
+// ****************************************************************************
+// *             Проинициализировать порядок использования кукисов            *
+// *   (для правильной оценки ситуации, одновременно ведутся: сессионная      *
+// *                  переменная и переменная-кукис)                          *
+// ****************************************************************************
+function ini_ResCookie()
+{
+   // Если кукиса 'ResCookie' нет, то будем указывать режим rciCookiNo" в обеих
+   // переменных и возвращаем данное значение.
+   // Сообщения об этом в подвале сайта выводится не будет.
+   // Данный режим предполагается по умолчанию.
+   prown\MakeSession('ResCookie',rciCookiNo,tInt);  
+   $Result=prown\MakeCookie('ResCookie',rciCookiNo,tInt);  
+   // Если сессионная переменная уже говорит, что кукисов нет, то переводим 
+   // или пытаемся перевести переменные в режим rciCookiNoMes.
+   if (isset($_SESSION['ResCookie'])&&($_SESSION['ResCookie']==rciCookiNo))
+   {
+      prown\MakeSession('ResCookie',rciCookiNoMes,tInt);  
+      $Result=prown\MakeCookie('ResCookie',rciCookiNoMes,tInt);  
+   }
+   // Существующий кукис взять, как есть. Если кукисов нет, то 
+   return $Result;
+}
+
+
+/*
+// Определяем порядок использования кукисов
+if (isset($_COOKIE['rciCookie']))
+{
+$с_ResCookie=prown\MakeCookie('ResCookie',rciCookiNo,tInt,true); // порядок использования кукисов 
+
+}
+else
+{
+$с_ResCookie=prown\MakeCookie('ResCookie',rciCookiNo,tInt,true); // порядок использования кукисов 
+}
+
+
+
+
+ и возможность работы JavaScript
+
+   //if (isset($_SESSION['js']))
+   //{
+   //   echo 'Есть JS'.'<br>';
+
+*/
+
+
+
+
+
 // Инициализируем переменные-кукисы
 $c_UserName=prown\MakeCookie('UserName',"Гость",tStr,true);      // логин авторизованного посетителя
 $c_PersName=prown\MakeCookie('PersName',"Гость",tStr,true);      // логин посетителя
 $c_BrowEntry=prown\MakeCookie('BrowEntry',0,tInt,true);          // число запросов сайта из браузера
 $c_PersEntry=prown\MakeCookie('PersEntry',0,tInt,true);          // счетчик посещений текущим посетителем
-$с_ResCookie=prown\MakeCookie('ResCookie',rciCookiNo,tInt,true); // порядок использования кукисов 
 
 $с_PageImg=prown\MakeCookie('PageImg','ittve01-001-Подъём-настроения.jpg',tStr,false); 
 //$c_isJScript=prown\MakeCookie('isJScript',7,tInt,false);        // JavaScript не включен
