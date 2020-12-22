a4222e200f2c9ec46fb41338a75823a574355d59
Vladimir-Trufanov <tve@karelia.ru>
2020-12-22 20:40:55 +0300

    Первый шаг по изучению setFetchMode()

--- www/DetmanBase/db_conn.php
+++ www/DetmanBase/db_conn.php
@@ -34,6 +34,7 @@ try
    print_r($rows); // здесь выводим данные
    */
    
+   /*
    // Второй вариант
    $db = new Db($pdo);
    $row = $db->queryRow('SELECT * FROM pages WHERE slug=:page LIMIT 1', array(':page' => $page));
@@ -43,7 +44,86 @@ try
       echo '<br>Просмотров: ' . $row['hits'] . '<br>';
       $db->update('pages', array('hits' => $row['hits'] + 1), 'id=:id', array(':id' => $row['id']));
    }
+   */
+   
+   // Для будущих тестов
+   /*
+   $pathBase='sqlite:'.$_SERVER['DOCUMENT_ROOT'].'/proba.db3';                                          
+   $pdo = new PDO($pathBase);
+   $st = $pdo->query('SELECT * FROM pages');
+   $results = $st->fetchAll(); 
+   foreach ($results as $row) {echo $row['id'].'-'; echo $row['text']; }
+   */ 
+
+   $pathBase='sqlite:'.$_SERVER['DOCUMENT_ROOT'].'/produkty1.db3'; 
+   $username='tve';
+   $password='23ety17';                                         
+   //$pdo = new PDO($pathBase);
+   $pdo = new PDO(
+      $pathBase, 
+      $username,
+      $password,
+      array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+   );
+   try 
+   {
+      $pdo->beginTransaction();
+      
+      $sql='CREATE TABLE vids ([id-vid] INTEGER PRIMARY KEY AUTOINCREMENT, vid TEXT)';
+      $st = $pdo->query($sql);
+      $sql='CREATE TABLE colours (
+         [id-colour] INTEGER PRIMARY KEY AUTOINCREMENT,
+         colour      TEXT
+      )';
+      $st = $pdo->query($sql);
+
+      $pdo->commit();
+   } 
+   catch (Exception $e) 
+   {
+      if ($pdo->inTransaction()) 
+      {
+         $pdo->rollback();
+         echo 'rollback';
+        // If we got here our two data updates are not in the database
+      }
+         echo 'throw';
+      //throw $e;
+   }
+
+   
+   /*
+   $sql='CREATE TABLE vids ([id-vid] INTEGER PRIMARY KEY AUTOINCREMENT, vid TEXT)';
+   $st = $pdo->query($sql);
+   $sql='CREATE TABLE colours (
+      [id-colour] INTEGER PRIMARY KEY AUTOINCREMENT,
+      colour      TEXT
+   )';
+   $st = $pdo->query($sql);
+   */
+
+   //$preparedStatement = $pdo->prepare($sql);
+   //$preparedStatement->execute();
+
+  
+   /* 
+   // Insert the metadata of the order into the database
+   $preparedStatement = $db->prepare(
+        'INSERT INTO `orders` (`order_id`, `name`, `address`, `created_at`)
+         VALUES (:name, :address, :telephone, :created_at)'
+   );
+   $preparedStatement->execute([
+        'name' => $name,
+        'address' => $address,
+        'telephone' => $telephone,
+        'created_at' => time(),
+   ]);
+   */
+
+
+   
    echo 'есть нашис контакты<br>';
+   
 }
 catch(Exception $e) 
 {
