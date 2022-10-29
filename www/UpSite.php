a7e256d1a2932633b992c1d0c4d488fa85d199ba
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2022-10-29 20:30:23 +0300

    Отлажено переключение стилей в одноколоночных режимах

--- www/UpSite.php
+++ www/UpSite.php
@@ -8,33 +8,8 @@
 
 //                                                   Автор:       Труфанов В.Е.
 //                                                   Дата создания:  13.01.2019
-// Copyright © 2019 tve                              Посл.изменение: 22.10.2022
+// Copyright © 2019 tve                              Посл.изменение: 29.10.2022
 
-// Формируем общие начальные теги разметки страницы
-echo '<!DOCTYPE html>';
-echo '<html lang="ru">';
-echo '<head>';
-echo '<meta http-equiv="content-type" content="text/html; charset=utf-8"/>';
-// SeoTags()
-echo '<title>Обо мне, путешествиях и ... Черногории</title>';
-echo '<meta name="description" content="Труфанов Владимир Евгеньевич, его жизнь и увлечения, жизнь его близких">';
-echo '<meta name="keywords" content="Труфанов Владимир Евгеньевич, жизнь и увлечения">';
-// Выводим данные о favicon
-echo '
-<link rel="manifest" href="manifest.json">
-<link rel="apple-touch-icon" sizes="180x180" href="/favicon260x260/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon260x260/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon260x260/favicon-16x16.png">
-<link rel="mask-icon" href="/favicon260x260/safari-pinned-tab.svg" color="#5bbad5">
-<link rel="shortcut icon" href="/favicon260x260/favicon.ico">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-config" content="/favicon260x260/browserconfig.xml">
-<meta name="theme-color" content="#ffffff">
-';
-// Подключаем font-awesome 4.7.0
-echo '<link rel="stylesheet"'.'href="font-awesome-4.7.0/css/font-awesome.min.css">';
-// Подключаем jQuery 2.2.4
-echo '<script src="jQuery/jquery-2.2.4.min.js"> </script>';
 /*
 // Подключаем TJsPrown и TJsTools
 echo '
@@ -48,6 +23,8 @@ echo '
 // Начинаем html-страницу
 echo '</head>'; 
 echo '<body>'; 
+
+
 
 // Проверяем не требуется ли просто вывести изображение и выводим его
 if ($ImageFile<>NULL)
@@ -108,10 +85,12 @@ else
       {
          echo '<div id="News">';
             //require_once "ittveNews/".$p_ittveNews;
+            require_once "ittveNews/IttveMe.html";
          echo '</div>';
          echo '<div id="Life">';
             require_once "ittveLife/".$p_ittveLife;
             ViewDebug($SiteDevice,$SiteRoot,$SiteAbove,$SiteHost);
+            echo '$urlHome='.$urlHome.'<br>';
             
             //echo  prown\getTranslit('Тезисы по организации сайта').'<br>';
             
--- www/UpSiteCSS.php
+++ www/UpSiteCSS.php
@@ -8,7 +8,33 @@
 
 //                                                   Автор:       Труфанов В.Е.
 //                                                   Дата создания:  11.11.2020
-// Copyright © 2020 tve                              Посл.изменение: 22.10.2022
+// Copyright © 2020 tve                              Посл.изменение: 29.10.2022
+
+// Формируем общие начальные теги разметки страницы
+echo '<!DOCTYPE html>';
+echo '<html lang="ru">';
+echo '<head>';
+echo '<meta http-equiv="content-type" content="text/html; charset=utf-8"/>';
+// SeoTags()
+echo '<title>Обо мне, путешествиях и ... Черногории</title>';
+echo '<meta name="description" content="Труфанов Владимир Евгеньевич, его жизнь и увлечения, жизнь его близких">';
+echo '<meta name="keywords" content="Труфанов Владимир Евгеньевич, жизнь и увлечения">';
+// Выводим данные о favicon
+echo '
+<link rel="manifest" href="manifest.json">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon260x260/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon260x260/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon260x260/favicon-16x16.png">
+<link rel="mask-icon" href="/favicon260x260/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/favicon260x260/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="/favicon260x260/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+';
+// Подключаем font-awesome 4.7.0
+echo '<link rel="stylesheet"'.'href="font-awesome-4.7.0/css/font-awesome.min.css">';
+// Подключаем jQuery 2.2.4
+echo '<script src="jQuery/jquery-2.2.4.min.js"> </script>';
 
 // Выполняем сброс стилей и устанавливаем начальные настройки стилей
 echo '<link rel="stylesheet" type="text/css" href="Styles/iniStyles.css">';
@@ -25,6 +51,89 @@ echo '<link rel="stylesheet" type="text/css" href="Styles/MenuArticles.css">';
 echo '<link rel="stylesheet" type="text/css" href="Styles/Footer-Info.css">';
 // Стилизуем панель меню управления в подвале страницы 
 echo '<link rel="stylesheet" type="text/css" href="Styles/MenuLeader.css">';
+// Определяем стили страницы редактирования материалов
+echo '<link rel="stylesheet" type="text/css" href="Styles/EditText.css">';
+// При одноколоночном режиме отключаем див '#News'
+if ($c_PresMode==rpmOneRight)
+{
+   echo "
+   <style>
+   #News
+   { 
+     right:0;
+     width:0; 
+   }
+   #Life,#Footer,#Info
+   { 
+      right:33%;
+      width:67%; 
+   }
+   #Gallery
+   {
+      margin-left:67%;
+      width:33%;
+   }
+   #LifeMenu
+   {
+      left:0;
+   }
+   #RightFooter
+   {
+      right:33%;
+   }
+   </style>
+   ";
+}
+else if ($c_PresMode==rpmOneLeft)
+{
+   echo "
+   <style>
+   #News
+   { 
+     right:0;
+     width:0; 
+   }
+   #Life,#Footer,#Info
+   { 
+      left:33%;
+      width:67%; 
+   }
+   #Gallery
+   {
+      margin-right:67%;
+      width:33%;
+   }
+   #LifeMenu
+   {
+      left:33.1%;
+   }
+   #RightFooter
+   {
+      right:0;
+   }
+   </style>
+   ";
+}
+else
+{
+   echo "
+   <style>
+#News
+{ 
+   right:33%;
+   width:10%; 
+}
+#Life
+{ 
+   right:43%;
+   width:57%; 
+}
+   </style>
+   ";
+}
+
+
+
 
 // Переключаем контекстные колонки
 if (prown\isComRequest('LifeMenu','Com'))
