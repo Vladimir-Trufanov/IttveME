87836ecf68a63960baff94d96bd54de3dba16301
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2022-11-24 21:54:46 +0300

    Первая версия Me-главного меню

--- www/MenuArticles.php
+++ www/MenuArticles.php
@@ -11,80 +11,6 @@
 // Copyright © 2020 tve                              Посл.изменение: 13.12.2020
 
 echo 'Жизнь и путешествия! '.prown\getTranslit('').' <br>';
-?>
-<ul class="accordion">
-
-   <li id="moya-zhizn" class="moya-zhizn"><a href="#moya-zhizn">Моя жизнь<span>495</span></a>
-   <ul class="sub-menu">
-      <li><a href="#osobennosti-ustrojstva-vintikov-v-moej-golove"><em>1</em>Особенности устройства винтиков в моей голове<span>01.02.2013</span></a></li>			
-   </ul>
-   </li>
-   <li id="mikroputeshestviya" class="mikroputeshestviya"><a href="#mikroputeshestviya">Микропутешествия<span>26</span></a>
-   <ul class="sub-menu">
-      <li><a href="#kindasovo-zemlya-karelskogo-yumora"><em>1</em>Киндасово - земля карельского юмора<span>20.06.2010</span></a></li>	
-      <li><a href="#gora-sampo-ozero-svetlyj-les-tropinka-v-nebo"><em>2</em>Гора Сампо. Озеро, светлый лес, тропинка в небо<span>23.06.2010</span></a></li>
-      
-      <!-- 
-		<li><a href="#part2"><em>3</em>Падозеро, кладбище заключенных лагеря №517<span>03.07.2010</span></a></li>
-		<li><a href="#part2"><em>4</em>Таёжный зоопарк на озере Сямозеро<span>04.07.2010</span></a></li>
-		<li><a href="#part2"><em>5</em>Шелтозер. Так жили вепсы<span>10.07.2010</span></a></li>
-		<li><a href="#part2"><em>6</em>Полоса 2300 - военный аэродром в Гирвасе<span>17.07.2010</span></a></li>
-		<li><a href="#part2"><em>8</em>Чертов стул, кусочек ботанического сада<span>11.09.2010</span></a></li>
-		<li><a href="#part2"><em>10</em>Благовещенский Ионо-Яшезерский мужской монастырь<span>10.10.2010</span></a></li>
-      -->
-   </ul>                                                                                     
-   </li>
-      <!-- -->
-      <!-- -->
-   <!-- 
-   <li id="part3" class="vsyakoe-raznoe">      
-   <a href="#part3">Всякое-разное<span>58</span></a>
-   <ul class="sub-menu">
-      <li><a href="#part3"><em>1</em>Всякое-разное<span>05.02.1958</span></a></li>
-	</ul>
-	</li>
-   
-   <li id="part4" class="v-kontakte">      
-   <a href="#part4">В контакте<span>58</span></a>
-   <ul class="sub-menu">
-      <li><a href="#part4"><em>1</em>В контакте<span>05.02.1958</span></a></li>
-	</ul>
-	</li>
-   
-   <li id="part5" class="moj-mir">       
-   <a href="#part5">Мой мир<span>58</span></a>
-   <ul class="sub-menu">
-      <li><a href="#part5"><em>1</em>Мой мир<span>05.02.1958</span></a></li>
-	</ul>
-	</li>
-   
-   <li id="part6" class="progulki">       
-   <a href="#part6">Прогулки<span>58</span></a>
-   <ul class="sub-menu">
-      <li><a href="#part6"><em>1</em>Прогулки<span>05.02.1958</span></a></li>
-	</ul>
-	</li>
-   
-   <li id="part22" class="dopolneniya-k-mikroputeshestviyam">       
-   <a href="#part22">Дополнения к микропутешествиям<span>58</span></a>
-   <ul class="sub-menu">
-      <li><a href="#part22"><em>1</em>Дополнения к микропутешествиям<span>05.02.1958</span></a></li>
-	</ul>
-	</li>
-   
-   <li id="part99" class="perepechatka">       
-   <a href="#part99">Перепечатка<span>58</span></a>
-   <ul class="sub-menu">
-      <li><a href="#part99"><em>1</em>Перепечатка<span>05.02.1958</span></a></li>
-	</ul>
-	</li>
-	-->	
-</ul>
-
-<?php
-
-echo '<br>';
-
 // Готовим и выводим меню
 $Arti=new ttools\ArticlesMaker($basename,$username,$password);
 
@@ -92,10 +18,8 @@ $Arti=new ttools\ArticlesMaker($basename,$username,$password);
 // $Arti->BaseFirstCreate();
 
 // Строим html-код меню по базе данных материалов сайта 
-// $FirstUl=' id="main-menu" class="sm sm-mint"';
-$FirstUl=' class="accordion"';
-echo "\nMakeMenuMe\n"; 
-$Arti->MakeMenu($FirstUl);
+//$Arti->ShowSampleMenu();  // из примера меню
+$Arti->MakeMenu();          // по базе
 
 /*
 echo '_MakeTblMenu<br>';
