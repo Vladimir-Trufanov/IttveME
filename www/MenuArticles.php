bcf27dbf97c13ab81b97452b16a121bdcf4348b3
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2022-11-30 17:08:10 +0300

    Два набора юникодов

--- www/MenuArticles.php
+++ www/MenuArticles.php
@@ -10,7 +10,24 @@
 //                                                   Дата создания:  13.12.2020
 // Copyright © 2020 tve                              Посл.изменение: 13.12.2020
 
-echo 'Жизнь и путешествия! '.prown\getTranslit('').' <br>';
+echo 'Жизнь и путешествия! '.prown\getTranslit('').'&#128152;&#129315;'.' <br>';
+
+
+
+$с1='&#128152;';
+$с2=htmlspecialchars($с1);
+$с3=htmlspecialchars_decode($с2);
+echo $с1.'.'.$с2.'.'.$с3.'.'.ord($с3).'<br>';
+echo strlen($с1).' --> '.strlen($с2).' --> '.strlen($с3).'<br>';
+
+$с1='&#129315;';
+$с2=htmlspecialchars($с1);
+$с33=htmlspecialchars_decode($с2);
+echo $с1.'.'.$с2.'.'.$с3.'<br>';
+echo strlen($с1).' --> '.strlen($с2).' --> '.strlen($с3).'<br>';
+
+if ($с3==$с33) echo ('Истина!'); else echo ('ЛОЖЬ!');
+
 // Готовим и выводим меню
 $Arti=new ttools\ArticlesMaker($basename,$username,$password);
 
