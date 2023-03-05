dbe9620912aa57ad748a4cec7391d7880542af17
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2023-03-05 08:27:09 +0300

    вчерне подключен хекстрис

--- www/ttools/TSaymeClass/Hextris/vendor/js.cookie.js
+++ www/ttools/TSaymeClass/Hextris/vendor/js.cookie.js
@@ -2,6 +2,8 @@
  * JavaScript Cookie v2.0.0-pre
  * https://github.com/js-cookie/js-cookie
  *
+ * Простой и легкий JavaScript API для обработки файлов cookie браузера.
+ * 
  * Copyright 2006, 2015 Klaus Hartl
  * Released under the MIT license
  */
