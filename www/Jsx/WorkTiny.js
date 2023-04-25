90f45886c0df5f5809ef20070be55098cac4ed8a
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2023-04-25 16:12:40 +0300

    Отлажена передача изображения для детализации

--- www/Jsx/WorkTiny.js
+++ www/Jsx/WorkTiny.js
@@ -55,50 +55,19 @@ $(document).ready(function()
       });
    });
 })
-
-   /*
-   //console.log(imime_type);
-   //let iuri=RootUrl+'?MimeType='+imime_type+'&DataPic='+iDataPic;
-   //console.log('iuri='+iuri);
-   let ImageFile='/Images/sampo.jpg';
-   let iuri=RootUrl+'?Image='+ImageFile;
-   //localStorage.setItem('MimeType',imime_type);
-   //localStorage.setItem('DataPic',iDataPic);
-   localStorage.setItem('DataPic',isSrc);
-   location.replace(iuri);
-   */
-
-   /*
-   $.ajax({
-	url: 'Probax.php',
-	method: 'post',
-	data: {Image:'ImageFile'},
-	success: function(data)
-   {
-		alert(data);
-	}
-   });
-   
-   */
-
-
-function ImageClick(isSrc)
+// ****************************************************************************
+// *       Переключиться на детальный просмотр выбранного изображения         *
+// ****************************************************************************
+function ImageClick(i2sSrc)
 {
-
-   let ImageFile='/Images/sampo.jpg';
+   // Готовим url для показа изображения
+   let ImageFile='img';
    let iuri=RootUrl+'?Image='+ImageFile;
-   /*
-   localStorage.setItem('DataPic',isSrc);
-   console.log(isSrc);
-   alert(isSrc);
-   console.log(localStorage.getItem("DataPic"));
-   alert(localStorage.getItem("DataPic"));
-   */
-   location.replace(iuri);
-
+   // Отправляем изображение в формате base64 в localStorage
+   localStorage.setItem('DataPic',i2sSrc);
+   // Переключаемся на страницу просмотра изображения
+   location.assign(iuri);
 }
-
-
 // ****************************************************************************
 // *       Отключить блокировку закрытия страницы редактирования материала    *
 // ****************************************************************************
--- www/Probax.php
+++
@@ -1,4 +0,0 @@
-<?php
-Header("Location: ".'index.php'.'?Image='.'Translit',true);
-echo 'Русский';
-exit;
