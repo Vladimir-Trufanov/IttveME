46c0cf32590fed5602766a507510e5f0e286da07
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2023-09-26 20:58:04 +0300

    Проба отправки писем

--- www/ttools/TEntryClass/EntryClass.php
+++ www/ttools/TEntryClass/EntryClass.php
@@ -39,18 +39,62 @@ class Entrying
    public function Body() 
    {
       
-      require_once "InteractiveSpooky.php"; 
-
+      //require_once "InteractiveSpooky.php"; 
+      //require_once "EmailRegistration.php"; 
       
       /*
-         echo 'Выполняем вход или регистрируемся на сайте!<br>';
-         echo '
-         ';
-         echo '
-         ';
+      // Сообщение
+      $message = "Line 1\r\nLine 2\r\nLine 3";
+      // На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+      $message = wordwrap($message, 70, "\r\n");
+      // Отправляем
+      mail('tve58@inbox.ru', 'My Subject', $message);
       */
       
-      //}
+      // несколько получателей
+      $to  = 'tve58@inbox.ru' . ', '; // обратите внимание на запятую
+      $to .= 'tve@karelia.ru';
+      // тема письма
+      $subject = 'Здесь тема письма: Напоминания о днях рождениях';
+      // текст письма
+      $message = '
+      <html>
+      <head>
+         <title>Напоминания о днях рождениях</title>
+      </head>
+      <body>
+         <p>Напоминания о днях рождениях</p>
+         <table>
+         <tr>
+            <th>Кто</th><th>День</th><th>Месяц</th><th>Год</th>
+         </tr>
+         <tr>
+            <td>Таня</td><td>1-го</td><td>января</td><td>1958</td>
+         </tr>
+         <tr>
+            <td>Лена</td><td>15-го</td><td>января</td><td>1981</td>
+         </tr>
+         <tr>
+            <td>Ксюша</td><td>20-го</td><td>января</td><td>1991</td>
+         </tr>
+         </table>
+      </body>
+      </html>
+      ';
+      // Для отправки HTML-письма должен быть установлен заголовок Content-type
+      $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+      // Дополнительные заголовки
+      // $headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+      $headers .= 'From: Дни Рождения <birthday@example.com>' . "\r\n";
+      //$headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+      //$headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+      
+      // Отправляем
+      $err=mail($to, $subject, $message, $headers);
+      if ($err) echo 'Письмо ушло!';
+      else echo 'Ошибка при отправке письма';
+
    }
 }
 // ********************************************************* EntryClass.php ***
